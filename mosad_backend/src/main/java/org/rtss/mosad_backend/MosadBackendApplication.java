53b3abf0099331f9e05896f8c26c3d150380bd18
sanjaya <katuwawala12365.usci.ruh.ac.lk>
2024-12-08 17:38:52 +0530

    feature/create_react_app_springboot_app

---
+++ mosad_backend/src/main/java/org/rtss/mosad_backend/MosadBackendApplication.java
@@ -0,0 +1,13 @@
+package org.rtss.mosad_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosadBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosadBackendApplication.class, args);
+	}
+
+}
