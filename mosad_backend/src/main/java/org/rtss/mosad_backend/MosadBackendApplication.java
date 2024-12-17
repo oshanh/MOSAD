e7451161a49d3d9e7c169422decb4b8f70b2084d
Tharaka Gamage <gamage12371@usci.ruh.ac.lk>
2024-12-17 21:54:42 +0530

    resolved_merge_conflict

--- mosad_backend/src/main/java/org/rtss/mosad_backend/MosadBackendApplication.java
+++ mosad_backend/src/main/java/org/rtss/mosad_backend/MosadBackendApplication.java
@@ -1,9 +1,6 @@
 package org.rtss.mosad_backend;
-
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MosadBackendApplication {
