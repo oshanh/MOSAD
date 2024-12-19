efdeae8ced6ddb644ee8d0988b7b714723cda9fd
sanjaya <katuwawala12365.usci.ruh.ac.lk>
2024-12-20 01:15:31 +0530

    feature/add_login_page

---
+++ mosad_frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const HomePage=()=>{
+    return(
+        <Typography>
+            Home page
+        </Typography>
+    )
+}
+
+export default HomePage
---
+++ mosad_frontend/src/services/apiUserService.js
@@ -0,0 +1,6 @@
+import apiClient from '../apiConfig/apiClient';
+
+
+
+
+
