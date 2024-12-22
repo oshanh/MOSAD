1d5e00d4d8589f1d073d9dbadd3916326600aee5
Oshan Harshad <oshanedu@gmail.com>
2024-12-22 13:43:49 +0530

    Update SecurityConfig.java

--- mosad_backend/src/main/java/org/rtss/mosad_backend/config/security/SecurityConfig.java
+++ mosad_backend/src/main/java/org/rtss/mosad_backend/config/security/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
             http
                     .authorizeHttpRequests((request) -> request
-                       .requestMatchers("/api/v1/login").permitAll()
+                       .requestMatchers("/api/v1/login","/api/v1/user/register").permitAll()
                        .anyRequest().authenticated()
                     )
                     //disabled the csrf token
