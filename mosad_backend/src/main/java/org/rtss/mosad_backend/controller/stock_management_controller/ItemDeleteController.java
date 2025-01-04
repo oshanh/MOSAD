e58ac8850b830984e3da98050fbc8dcb83c91435
Thisal Dilmith <tdilmithk20@gmail.com>
2025-01-04 13:46:04 +0530

    deletion+exception

--- mosad_backend/src/main/java/org/rtss/mosad_backend/controller/stock_management_controller/ItemDeleteController.java
+++ mosad_backend/src/main/java/org/rtss/mosad_backend/controller/stock_management_controller/ItemDeleteController.java
@@ -1,5 +1,6 @@
 package org.rtss.mosad_backend.controller.stock_management_controller;
 
+import org.rtss.mosad_backend.exceptions.ItemDeletionException;
 import org.rtss.mosad_backend.service.stock_management_service.ItemDeleteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,24 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/delete")
 public class ItemDeleteController {
 
-    private final ItemDeleteService ItemDeleteService;
+    private final ItemDeleteService itemDeleteService;
 
     @Autowired
-    public ItemDeleteController(ItemDeleteService ItemDeleteService) {
-        this.ItemDeleteService = ItemDeleteService;
+    public ItemDeleteController(ItemDeleteService itemDeleteService) {
+        this.itemDeleteService = itemDeleteService;
     }
 
     @DeleteMapping
-    public ResponseEntity<?> deleteItem(
+    public ResponseEntity<String> deleteItem(
             @RequestParam String category,
-            @RequestParam(required = false) String brand,
-            @RequestParam(required = false) Integer itemId) {
+            @RequestParam String brand,
+            @RequestParam Integer itemId) {
         try {
-            boolean isDeleted = ItemDeleteService.deleteItem(category, brand, itemId);
-            return isDeleted ? ResponseEntity.ok("Item deleted successfully.") :
-                    ResponseEntity.notFound().build();
+            itemDeleteService.deleteItem(category, brand, itemId);
+            return ResponseEntity.ok("Item deleted successfully.");
         } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            throw new ItemDeletionException("Invalid input provided: " + e.getMessage());
         }
     }
 }
