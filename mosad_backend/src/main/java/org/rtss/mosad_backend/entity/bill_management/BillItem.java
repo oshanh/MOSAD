ca11212365b3aa509f0f7e003bb3485abe30182e
Oshan Harshad <oshanedu@gmail.com>
2025-01-13 00:22:23 +0530

    Bill entity updated

--- mosad_backend/src/main/java/org/rtss/mosad_backend/entity/bill_management/BillItem.java
+++ mosad_backend/src/main/java/org/rtss/mosad_backend/entity/bill_management/BillItem.java
@@ -1,22 +1,34 @@
 package org.rtss.mosad_backend.entity.bill_management;
 
 import jakarta.persistence.*;
+import org.rtss.mosad_backend.entity.stock_management_entity.Item;
 
 @Entity
 @Table(name = "bill_items")
 public class BillItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
-    private String description;
+    @OneToOne
+    @JoinColumn(name="item_id",referencedColumnName = "itemId")
+    private Item item;
     private Integer quantity;
     private Double unitPrice;
-    private Double subtotal;
+
 
     @ManyToOne
     @JoinColumn(name = "bill_id", nullable = false)
     private Bill bill;
+
+    public BillItem() {
+    }
+
+    public BillItem(Item item, Integer quantity, Double unitPrice, Bill bill) {
+        this.item = item;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+        this.bill = bill;
+    }
 
     // Getters and Setters
 
@@ -28,13 +40,6 @@ public class BillItem {
         this.bill = bill;
     }
 
-    public Double getSubtotal() {
-        return subtotal;
-    }
-
-    public void setSubtotal(Double subtotal) {
-        this.subtotal = subtotal;
-    }
 
     public Double getUnitPrice() {
         return unitPrice;
@@ -52,13 +57,6 @@ public class BillItem {
         this.quantity = quantity;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 
     public Long getId() {
         return id;
@@ -68,14 +66,20 @@ public class BillItem {
         this.id = id;
     }
 
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
     @Override
     public String toString() {
         return "BillItem{" +
                 "id=" + id +
-                ", description='" + description + '\'' +
                 ", quantity=" + quantity +
                 ", unitPrice=" + unitPrice +
-                ", subtotal=" + subtotal +
                 ", bill=" + bill +
                 '}';
     }
