045fe27a1f285032b9e395aaa282ea22ebc1c7f1
Oshan Harshad <oshanedu@gmail.com>
2025-01-23 17:14:35 +0530

    lombok removed

--- mosad_backend/src/main/java/org/rtss/mosad_backend/dto/stock_management_dto/ItemTyreDTO.java
+++ mosad_backend/src/main/java/org/rtss/mosad_backend/dto/stock_management_dto/ItemTyreDTO.java
@@ -1,15 +1,54 @@
 package org.rtss.mosad_backend.dto.stock_management_dto;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class ItemTyreDTO {
-    private Long itemId;
-    private String tyreSize;
-    private String pattern;
-    private String vehicleType;
 
+    private Long itemId;
+    @NotBlank(message = "Tyre size cannot be blank")
+    @Size(max = 50, message = "Tyre size cannot exceed 50 characters")
+    public String tyreSize;
+
+    @NotBlank(message = "Pattern cannot be blank")
+    @Size(max = 100, message = "Pattern cannot exceed 100 characters")
+    public String pattern;
+
+    @NotBlank(message = "Vehicle type cannot be blank")
+    @Size(max = 50, message = "Vehicle type cannot exceed 50 characters")
+    public String vehicleType;
+
+
+    // Getters and Setters
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getTyreSize() {
+        return tyreSize;
+    }
+
+    public void setTyreSize(String tyreSize) {
+        this.tyreSize = tyreSize;
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
 }
--- mosad_backend/src/main/java/org/rtss/mosad_backend/entity/stock_management_entity/Item.java
+++ mosad_backend/src/main/java/org/rtss/mosad_backend/entity/stock_management_entity/Item.java
@@ -3,13 +3,6 @@ package org.rtss.mosad_backend.entity.stock_management_entity;
 import jakarta.persistence.*;
 import java.util.Set;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "item")
 public class Item {
@@ -43,5 +36,92 @@ public class Item {
 
 
 
+    public Item() {
+    }
+
+    public Item(String itemName, String itemDescription, double companyPrice, double retailPrice, double discount, Category category, Brand brand, Set<ItemBranch> itemBranches) {
+
+        this.itemName = itemName;
+        this.itemDescription = itemDescription;
+        this.companyPrice = companyPrice;
+        this.retailPrice = retailPrice;
+        this.discount = discount;
+        this.category = category;
+        this.brand = brand;
+        this.itemBranches = itemBranches;
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+
+    public double getCompanyPrice() {
+        return companyPrice;
+    }
+
+    public void setCompanyPrice(double companyPrice) {
+        this.companyPrice = companyPrice;
+    }
+
+    public double getRetailPrice() {
+        return retailPrice;
+    }
+
+    public void setRetailPrice(double retailPrice) {
+        this.retailPrice = retailPrice;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+
+    public Set<ItemBranch> getItemBranches() {
+        return itemBranches;
+    }
+
+    public void setItemBranches(Set<ItemBranch> itemBranches) {
+        this.itemBranches = itemBranches;
+    }
 
 }
