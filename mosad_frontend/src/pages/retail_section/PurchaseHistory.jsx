3188c1bed29b4d747fe6829d1efb950caeb64edd
sanjaya <katuwawala12365.usci.ruh.ac.lk>
2025-02-19 11:34:37 +0530

    Recongidured?purchase_history

--- mosad_frontend/src/pages/retail_section/PurchaseHistory.jsx
+++ mosad_frontend/src/pages/retail_section/PurchaseHistory.jsx
@@ -1,8 +1,9 @@
 import React, { useState ,useEffect } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import { Box, Typography } from "@mui/material";
-import useFetchPurchaseHistory from "../../hooks/servicesHook/useRetailService"
-import useAuth from "../../hooks/useAuth";
+import {useFetchPurchaseHistory} from "../../hooks/servicesHook/useRetailService";
+import useAuth from "../../hooks/useAuth";;
+
 const PurchaseHistory = () => {
     const fetchPurchaseHistory=useFetchPurchaseHistory();
     const [rows, setRows] = useState([]);
