5cfe42b8d5ddb0f6b29a5115975c626ff8971690
sanjaya <katuwawala12365.usci.ruh.ac.lk>
2025-02-20 09:54:44 +0530

    removed/Users_form_validation

--- mosad_frontend/src/pages/users_section/AllUsersView.jsx
+++ mosad_frontend/src/pages/users_section/AllUsersView.jsx
@@ -45,17 +45,7 @@ const columns = [
     { field: 'role',headerName: 'Role',width: 120,},
 ];
 
-const validateEmail = (email) => {
-    // More robust email validation regex
-    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return regex.test(email);
-  };
-  
-const validateContactNum = (contactNum) => {
-    // Basic phone number validation regex (numbers and optional hyphens/spaces)
-    const regex = /^[+]?[(]?\d{3}[)]?[-\s.]?\d{3}[-\s.]?\d{4,6}$/im;
-    return regex.test(contactNum);
-}
+
 
 const AllUsersView=()=>{
     const [errors,setErrors]=useState(initialErrors);
@@ -66,84 +56,7 @@ const AllUsersView=()=>{
     //control data loading asynchronus nature 
     const [isLoading, setIsLoading] = useState(false);
 
-    const validateForm = () => {
-        let newErrors = { ...initialErrors };
-        let isValid = true;
-    
-        if (!userUpdateData.userDto.firstName) {
-          newErrors.firstName = 'First name is required';
-          isValid = false;
-        } else if (userUpdateData.userDto.firstName.length < 2) {
-            newErrors.firstName = 'First name must be at least 2 characters';
-            isValid = false;
-        }
-    
-        if (!userUpdateData.userDto.lastName) {
-          newErrors.lastName = 'Last name is required';
-          isValid = false;
-        } else if (userUpdateData.userDto.lastName.length < 2) {
-            newErrors.lastName = 'Last name must be at least 2 characters';
-            isValid = false;
-        }
-    
-        if (!userUpdateData.userDto.username) {
-          newErrors.username = 'Username is required';
-          isValid = false;
-        } else if (userUpdateData.userDto.username.length < 4) {
-            newErrors.username = 'Username must be at least 4 characters';
-            isValid = false;
-        }
-    
-        if (!userUpdateData.userDto.email) {
-          newErrors.email = 'Email is required';
-          isValid = false;
-        } else if (!validateEmail(userUpdateData.userDto.email)) {
-          newErrors.email = 'Invalid email format';
-          isValid = false;
-        }
-    
-        if (userUpdateData.userContactDto.length > 0) {
-            userUpdateData.userContactDto.forEach((contact, index) => {
-                if (contact.contactNum && !validateContactNum(contact.contactNum)) {
-                    newErrors.contactNum = `Invalid contact number format at index ${index + 1}`;
-                    isValid = false;
-                }
-            });
-        }
-    
-    
-        if (!userUpdateData.userRoleDto.roleName) {
-          newErrors.roleName = 'User role is required';
-          isValid = false;
-        }
-    
-         //Only validate passwords if we are on the correct page
-         if (location.pathname === '/user/view-all') {
-          if (!pwds.pwd_1) {
-            newErrors.pwd_1 = 'Password is required';
-            isValid = false;
-          } else if (pwds.pwd_1.length < 8) {
-              newErrors.pwd_1 = 'Password must be at least 8 characters';
-              isValid = false;
-          }
-    
-          if (!pwds.pwd_2) {
-            newErrors.pwd_2 = 'Re-enter password is required';
-            isValid = false;
-          }  else if (pwds.pwd_2.length < 8) {
-              newErrors.pwd_2 = 'Password must be at least 8 characters';
-              isValid = false;
-          }
-    
-          if (pwds.pwd_1 !== pwds.pwd_2) {
-            newErrors.pwd_2 = 'Passwords do not match';
-            isValid = false;
-          }
-        }
-    
-        setErrors(newErrors);
-        return isValid;
-      };
+
 
     //fetch all users data
     const loadAllUsers=()=>{
