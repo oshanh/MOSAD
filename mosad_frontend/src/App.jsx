2c647959e862e8d7fca482569d513f0ddab35fac
sanjaya <katuwawala12365.usci.ruh.ac.lk>
2025-02-20 23:24:09 +0530

    merged_dilmith_works

--- mosad_frontend/src/App.jsx
+++ mosad_frontend/src/App.jsx
@@ -17,7 +17,7 @@ const AllBillsPage = lazy(() => import('./pages/bill_section/AllBillsPage'));
 const CreditPage=lazy(()=>import('./pages/credit_section/CreditPage'));
 const DackPage =lazy(()=>import( './pages/dack_section/DackPage'));
 const EmployeePage =lazy(()=>import( './pages/employee_section/EmployeePage'));
-// const ReportPredictionPage =lazy(()=>import( './pages/prediction_report_section/ReportPredictionPage'));
+const SalesReport =lazy(()=>import( './pages/report_section/SalesReport'));
 const RetailPageLayout =lazy(()=>import( './pages/retail_section/layout/RetailPageLayout'));
 const BillSectionLayout =lazy(()=>import('./pages/bill_section/BillSectionLayout'));
 const ServicesPage =lazy(()=>import( './pages/services_section/ServicesPage'));
@@ -64,7 +64,7 @@ function App() {
         <Route path='/login' element={auth.Authenticated ? <Navigate to="/home" replace /> : <LoginPage/>} />
         <Route element={<RoutesProtector />}>
           <Route element={<HomeLayout/>}>
-              <Route path="/home" element={ <HomePage />} />
+            <Route path="/home" element={ <HomePage />} />
 
             <Route element={<CheckPrivileges allowedRoles={["OWNER","ADMIN","STOCK_MANAGER"]}/>}>
               <Route path="/stock" element={ <StockPageLayout />} >
@@ -92,10 +92,9 @@ function App() {
               <Route path="/bill" element={<BillSectionLayout />} >
                 <Route index element={<BillPage />} />
                 <Route path="AllBillsPage" element={<AllBillsPage />} />
-
               </Route>
               <Route path="/dack" element={<DackPage />} />
-              {/* <Route path="/future" element={ <ReportPredictionPage />} /> */}
+              <Route path="/future" element={ <SalesReport />} />
               <Route path="/services" element={<ServicesPage />} />
             </Route>
 
@@ -105,23 +104,6 @@ function App() {
                   <Route path="purchase-history" element={ <PurchaseHistory />} />
                   <Route path="incomplete-transactions" element={ <IncompleteTransactions />} />
                   <Route path="product-availability" element={ <FindProductAvailability />} />
-              </Route>
-
-              <Route element={<CheckPrivileges allowedRoles={["OWNER","ADMIN"]}/>}>
-                <Route path="/credit" element={ <CreditPage />} />
-                <Route path="/bill" element={ <BillPage />} />
-                <Route path="/dack" element={ <DackPage />} />
-                <Route path="/future" element={ <ReportPredictionPage />} />
-                <Route path="/services" element={ <ServicesPage />} />
-              </Route>
-
-              <Route element={<CheckPrivileges allowedRoles={["OWNER","ADMIN","RETAIL_CUSTOMER"]}/>}>
-                <Route path="/retail" element={ <RetailPageLayout />} >
-                    <Route index element={ <PaymentHistory />} />
-                    <Route path="purchase-history" element={ <PurchaseHistory />} />
-                    <Route path="incomplete-transactions" element={ <IncompleteTransactions />} />
-                    <Route path="product-availability" element={ <ProductAvailabilityChecker />} />
-                </Route>
               </Route>
 
             <Route element={<CheckPrivileges allowedRoles={["OWNER","ADMIN","STOCK_MANAGER","BRANCH_MANAGER","MECHANIC"]}/>}>
@@ -138,6 +120,7 @@ function App() {
               </Route>
            </Route>
 
+        </Route>
         </Route>
         </Route>
       </Routes>
---
+++ mosad_frontend/src/pages/report_section/StockReport.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import UnderConstruction from '../../component/UnderConsruction'
+
+const StockReport = () => {
+  return (
+     <UnderConstruction displayTxt={<p> Employee page is under constuction</p>}/>  
+  )
+}
+
+export default StockReport
