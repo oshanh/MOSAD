a9b35f578bc10973af7a304fcdfe294ef31d2b84
sanjaya <katuwawala12365.usci.ruh.ac.lk>
2025-02-24 20:52:47 +0530

    added/cors_origin_environemt_variables

--- mosad_frontend/src/services/api_config/apiClient.js
+++ mosad_frontend/src/services/api_config/apiClient.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL= 'http://localhost:8080/api/v1';
+const BASE_URL= import.meta.env.VITE_API_URL
 
 export const apiClient = axios.create({
     baseURL:BASE_URL,
