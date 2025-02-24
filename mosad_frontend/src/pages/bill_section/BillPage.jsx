bdcb02ba9c88711d7696183481a766b87095a5ec
Oshan Harshad <oshanedu@gmail.com>
2025-02-24 18:50:15 +0530

    Bug fixes

--- mosad_frontend/src/pages/bill_section/BillPage.jsx
+++ mosad_frontend/src/pages/bill_section/BillPage.jsx
@@ -191,12 +191,14 @@ const handleCreateCredit = async (creditData) => {
       x: 10, // Set starting position for content
       y: 10,
       html2canvas: {
-        scale: 0.22, // Try a higher scale value to capture the content better
+        scale: 0.1, // Try a higher scale value to capture the content better
         width: 250, // Ensure the content uses the full width of the page (A4 size)
         height: 297, // Ensure full height is captured (A4 size)
       },
     });
-    //clearAllFields();
+    setTimeout(() => {
+      clearAllFields();
+    }, 2000);
   };
   
   
