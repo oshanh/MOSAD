ded420b3a3b01f3b9469205e7ba6545898853f7a
sanjaya <katuwawala12365.usci.ruh.ac.lk>
2025-03-01 20:50:13 +0530

    Created/role_based_jwt_tokens

--- mosad_backend/src/main/java/org/rtss/mosad_backend/dto/user_dtos/AuthDTO.java
+++ mosad_backend/src/main/java/org/rtss/mosad_backend/dto/user_dtos/AuthDTO.java
@@ -9,16 +9,10 @@ public class AuthDTO {
     private boolean authenticated;
     @JsonProperty("access_token")
     private String accessToken;
-    @JsonProperty("role")
-    private String role;
-    @JsonProperty("branchId")
-    private Long branchId;
 
-    public AuthDTO(boolean authenticated, String accessToken, String role, Long branchId) {
+    public AuthDTO(boolean authenticated, String accessToken) {
         this.authenticated = authenticated;
         this.accessToken = accessToken;
-        this.role = role;
-        this.branchId = branchId;
     }
 
     public AuthDTO() {
@@ -40,29 +34,4 @@ public class AuthDTO {
         this.authenticated = authenticated;
     }
 
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public Long getBranchId() {
-        return branchId;
-    }
-
-    public void setBranchId(Long branchId) {
-        this.branchId = branchId;
-    }
-
-    @Override
-    public String toString() {
-        return "AuthDTO{" +
-                "authenticated=" + authenticated +
-                ", accessToken='" + accessToken + '\'' +
-                ", role='" + role + '\'' +
-                ", branchID='" + branchId + '\'' +
-                '}';
-    }
 }
